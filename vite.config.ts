ee553e5eb2880b8054e196eea738731574dea5a8
iamgvnrc <iamgvnrc@aol.com>
2026-08-23 14:17:52 -0400

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
-  plugins: [
+  base: '/hakyhm-web/',  plugins: [
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
